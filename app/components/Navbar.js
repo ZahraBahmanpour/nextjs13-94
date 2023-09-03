ca192a2ff4a5eb95bfe86ead50e47b6253357faa
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-03 09:48:12 +0330

    add Image to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import Logo from "../../public/next.svg";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt="next logo" width={70} quality={100} />
       <Link href={"/"}>Dashboard</Link>
       <Link href={"/tickets"}>Tickets</Link>
     </nav>
