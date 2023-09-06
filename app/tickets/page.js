11b22f79565153e40242cd74bbe703bd50bd242e
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-06 12:11:04 +0330

    add data fetching with cache

--- app/tickets/page.js
+++ app/tickets/page.js
@@ -1,7 +1,24 @@
-export default function Tickets() {
+async function getPosts() {
+  const res = await fetch("http://localhost:3000/api/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Tickets() {
+  const posts = await getPosts();
   return (
     <main>
-      <h2>Tickets</h2>
+      <h2>Posts</h2>
+      {posts.map((post) => (
+        <div className="card">
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </main>
   );
 }
